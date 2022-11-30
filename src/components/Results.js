1554990fc06359f5811b15099a5b736b4d30b973
martenj <marten@jalakas.ee>
2022-11-30 21:10:41 +0200

    add filtering out non-unique anagrams

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 export default function Results(props) {
+  const originalResults = props.searchResults.map(
+    (result) => result["original_word"]
+  );
+
+  // Filters out non-unique results
+  const uniqueResults = [...new Set(originalResults)];
+
   return (
     <div className="flex flex-col items-center top-10">
       <table className="bg-rose-200">
@@ -10,9 +17,9 @@ export default function Results(props) {
           </tr>
         </thead>
         <tbody>
-          {props.searchResults.map((result) => (
+          {uniqueResults.map((result) => (
             <tr className="border-b-2 border-black">
-              <td className="text-center">{result["original_word"]}</td>
+              <td className="text-center">{result}</td>
             </tr>
           ))}
         </tbody>
